5be10ff96c55597e0af6fcad302074306dbb1e4a
Gabriel Polo Brallard <polo1@avaya.com>
2020-05-06 12:29:54 -0300

    chage uri db config

--- config/db.js
+++ config/db.js
@@ -2,5 +2,6 @@
 // To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
 
 module.exports = {
-  db: process.env.DB
+  // db: process.env.DB
+  db: 'mongodb://db/mern-crud',
 };
